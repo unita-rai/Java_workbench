f2cc762953eb9732e3d07ab67ca36e91e0f0e528
unita-rai <unita.rai@gmail.com>
2026-02-26 10:46:31 +0000

    01/Iteration:VAriablen deklarien

---
+++ com/cc/java/Bestellung.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Bestellung {
+
+    public static void main(String[] args) {
+       
+        // Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+
+        System.out.println(food);
+        System.out.println(drink);
+
+   
+
+
+    }
+}
